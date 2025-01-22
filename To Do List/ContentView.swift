c56d59bde7bd7dd1b6b17b1f7372746e230012b3
Zane <zmatarieh@bsd220.org>
2025-01-22 09:35:50 -0600

    Initial Commit

---
+++ To Do List/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  To Do List
+//
+//  Created by Zane Matarieh on 1/22/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ To Do ListTests/To_Do_ListTests.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_ListTests.swift
+//  To Do ListTests
+//
+//  Created by Zane Matarieh on 1/22/25.
+//
+
+import Testing
+@testable import To_Do_List
+
+struct To_Do_ListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
